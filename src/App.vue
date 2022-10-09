4b1a35895c3b99c43cc234f18116a76776393fb5
Yusuf <priler001@gmail.com>
2022-10-09 12:46:59 +0300

    list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <h1>Helloworld</h1>
+  <post-form></post-form>
+  <post-form></post-form>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PostList from "./components/PostList.vue";
+import PostForm from "./components/PostForm.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    PostList,
+    PostForm
   },
+  data() {
+    return {
+      posts: [
+        { id: 1, name: "hello", description: "Hello my name" },
+        { id: 2, name: "hello1", description: "Hello my name is Yusuf"},
+        { id: 3, name: "hello2", description: "Hello my name43"},
+      ]
+    }
+  }
 };
 </script>
 
---
+++ src/components/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><strong>Название</strong> {{}}<div/>
+</template>
+
+<script>
+export default {
+  name: "PostList"
+}
+</script>
+
+<style scoped>
+
+</style>
